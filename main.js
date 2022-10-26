ee3229f03035ea0de9cfb352c47f8425814b4b0e
Abhimanyu000 <89971842+Abhimanyu000@users.noreply.github.com>
2022-10-26 12:41:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas=createCanvas(600, 350);
+    canvas.center();
+}
+
+function draw(){
+
+}
+
+function clickImg(){
+    save("filteredIMAGE.png");
+}
