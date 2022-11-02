f64af6ac2f850adecb6840e1c0ae6080f12ac7d9
Abhimanyu000 <89971842+Abhimanyu000@users.noreply.github.com>
2022-11-02 19:27:23 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-function preload(){
+poseX=0;
+poseY=0;
 
+function preload(){
+    mustache=loadImage("https://i.postimg.cc/3x3QzSGq/m.png");
 }
 
 function setup(){
@@ -14,6 +17,7 @@ function setup(){
 
 function draw(){
     image(video, 0, 0, 600, 350);
+    image(mustache, poseX, poseY, 70, 37);
 }
 
 function clickImg(){
@@ -27,7 +31,9 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length>0){
         console.log(results);
-        console.log("nose x"+results[0].pose.nose.x);
-        console.log("nose y"+results[0].pose.nose.y);
+        poseX=results[0].pose.nose.x-35;
+        poseY=results[0].pose.nose.y-10;
+        console.log("nose x"+poseX);
+        console.log("nose y"+poseY);
     }
 }
